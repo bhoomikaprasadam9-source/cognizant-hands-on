a5fbf9f1c507ad1fa73f8f183388d19fec7d006e
bhoomikaprasadam9-source <bhoomikaprasadam9@gmail.com>
2026-07-25 21:42:23 +0530

    added country web service

---
+++ week3/country-web-service/src/main/java/controller/CountryController.java
@@ -0,0 +1,21 @@
+package com.cognizant.countrywebservice.controller;
+
+import com.cognizant.countrywebservice.Country;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        return context.getBean("country", Country.class);
+
+    }
+}
