c7698a54a35c3f0b54ce894ec2430af64caee265
bhoomikaprasadam9-source <bhoomikaprasadam9@gmail.com>
2026-07-25 19:48:51 +0530

    added load country

--- week3/load country/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ week3/load country/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 @SpringBootApplication
 public class SpringLearnApplication {
 
@@ -16,5 +19,21 @@ public class SpringLearnApplication {
 		SpringApplication.run(SpringLearnApplication.class, args);
 
 		LOGGER.info("Inside main");
+
+		displayCountry();
+
 	}
+
+	public static void displayCountry() {
+
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("country.xml");
+
+		Country country =
+				context.getBean("country", Country.class);
+
+		LOGGER.debug("Country : {}", country);
+
+	}
+
 }
