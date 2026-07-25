175b9cc073863d01c57a9c68bf27fd4dc424d0b3
bhoomikaprasadam9-source <bhoomikaprasadam9@gmail.com>
2026-07-25 20:26:26 +0530

    added spring learn rest

---
+++ week3/spring-learn-rest/src/test/java/com/cognizant/springlearnrest/SpringLearnRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearnrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
