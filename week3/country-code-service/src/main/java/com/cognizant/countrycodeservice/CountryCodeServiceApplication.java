ccb39698504f9ed5e758de9a1d764e4f7b8de9ad
bhoomikaprasadam9-source <bhoomikaprasadam9@gmail.com>
2026-07-25 22:17:34 +0530

    added country code service

---
+++ week3/country-code-service/src/main/java/com/cognizant/countrycodeservice/CountryCodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.countrycodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryCodeServiceApplication.class, args);
+	}
+
+}
